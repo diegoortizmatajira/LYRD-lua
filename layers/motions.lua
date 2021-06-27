b199be7a951bf438c84e026abb8fe47316bebc44
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-06-27 12:14:05 -0500

    change easymotion for hop

--- layers/motions.lua
+++ layers/motions.lua
@@ -1,39 +1,30 @@
 local setup = require "LYRD.setup"
 local mappings = require "LYRD.layers.mappings"
+local commands = require "LYRD.layers.commands"
+local c = commands.command_shortcut
 
 local L = {name = 'Motions'}
 
-function L.plugins(s) setup.plugin(s, {'easymotion/vim-easymotion'}) end
+function L.plugins(s) setup.plugin(s, {
+    'phaazon/hop.nvim'
+}) end
 
-function L.settings(s) end
+function L.settings(s)
+    require('hop').setup {
+        keys = "fjdksla;gh"
+    }
+end
 
 function L.keybindings(s)
-    -- Hides the whichkey menu for <Leader><Leader>
-    mappings.leader_ignore_key(s, {','})
-
+    mappings.leader(s, {
+        {'', {'l'}, c('HopLine'), 'Hop to line'},
+        {'', {'w'}, c('HopWord'), 'Hop to word'},
+        {'', {','}, c('HopPattern'), 'Hop to pattern'},
+    })
     mappings.keys(s, {
-        {'', '<Leader><Leader>', '<Plug>(easymotion-prefix)'},
-        {'', '/', '<Plug>(easymotion-sn)'},
-        {'o', '/', '<Plug>(easymotion-sn)'},
-        -- ➡ Jump forward
-        {'', '<Plug>(easymotion-prefix)l', '<Plug>(easymotion-lineforward)'},
-        -- ⬇ Jump below (Start of line)
-        {'', '<Plug>(easymotion-prefix)j', '<Plug>(easymotion-sol-j)'},
-        -- ⬇ Jump below
-        {'', '<Plug>(easymotion-prefix)J', '<Plug>(easymotion-j)'},
-        -- ⬆ Jump above (Start of line)
-        {'', '<Plug>(easymotion-prefix)k', '<Plug>(easymotion-sol-k)'},
-        -- ⬆ Jump above
-        {'', '<Plug>(easymotion-prefix)K', '<Plug>(easymotion-k)'},
-        -- ⬅ Jump backward
-        {'', '<Plug>(easymotion-prefix)h', '<Plug>(easymotion-linebackward)'},
-        -- Jump anywhere
-        {'', '<Plug>(easymotion-prefix)a', '<Plug>(easymotion-jumptoanywhere)'},
-        -- Find character (In line)
-        {'', '<Plug>(easymotion-prefix)s', '<Plug>(easymotion-s)'},
-        -- Search expression (In line)
-        {'', '<Plug>(easymotion-prefix)S', '<Plug>(easymotion-sln)'}
-    }, {noremap = false, silent = true})
+        {'', 's', '<cmd>HopChar1<CR>'},
+        {'', 'S', '<cmd>HopChar2<CR>'}
+    })
 end
 
 return L
