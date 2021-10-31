269df8e436d966baff4bde78d42e03f19acaf02d
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-10-31 08:51:17 -0500

    Update filetree settings

--- layers/filetree.lua
+++ layers/filetree.lua
@@ -4,34 +4,37 @@ local commands = require"LYRD.layers.commands"
 local L = {name = 'File tree'}
 
 function L.plugins(s)
-  setup.plugin(s, {'kyazdani42/nvim-web-devicons', 'kyazdani42/nvim-tree.lua'})
+    setup.plugin(s, {'kyazdani42/nvim-web-devicons', 'kyazdani42/nvim-tree.lua'})
 end
 
 function L.settings(s)
-  require'nvim-tree'.setup{
-    disable_netrw = false,
-    auto_close = true,
-    update_cwd = true,
-    diagonostics = {enable = true},
-    view = {width = 60, side = 'right'}
+    require'nvim-tree'.setup{
+        disable_netrw = false,
+        auto_close = true,
+        update_cwd = true,
+        diagonostics = {enable = true},
+        view = {width = 60, side = 'right'},
+        filters = {
+            dotfiles = false,
+            custom = {'.git', 'node_modules', '.cache', 'bin', 'obj'}
+        }
+    }
+    vim.g.nvim_tree_quit_on_open = 1
+    vim.g.nvim_tree_gitignore = 1
+    vim.g.nvim_tree_icons = {
+        default = '',
+        symlink = '',
+        git = {
+            unstaged = "\u{f8eb}",
+            staged = "\u{f8ec}",
+            unmerged = "\u{f5f7}",
+            renamed = "\u{f45a}",
+            untracked = "\u{f893}"
+        },
+        folder = {default = "", open = "", symlink = ""}
+    }
 
-  }
-  vim.g.nvim_tree_quit_on_open = 1
-  vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', 'bin', 'obj'}
-  vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-      unstaged = "\u{f8eb}",
-      staged = "\u{f8ec}",
-      unmerged = "\u{f5f7}",
-      renamed = "\u{f45a}",
-      untracked = "\u{f893}"
-    },
-    folder = {default = "", open = "", symlink = ""}
-  }
-
-  commands.implement(s, '*', {LYRDViewFileTree = ':NvimTreeToggle', LYRDViewFileExplorer = ':Ntree'})
+    commands.implement(s, '*', {LYRDViewFileTree = ':NvimTreeToggle', LYRDViewFileExplorer = ':Ntree'})
 end
 
 return L
