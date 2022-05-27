e6248ab11e8c6a2f7f40658acdb9d937f7c45356
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-05-27 18:08:01 -0400

    Remove autoformat plugin

--- layers/dev.lua
+++ layers/dev.lua
@@ -4,24 +4,13 @@ local setup = require"LYRD.setup"
 local L = {name = 'Development'}
 
 function L.plugins(s)
-  setup.plugin(s, {
-    'tpope/vim-commentary',
-    'vim-autoformat/vim-autoformat',
-    'norcalli/nvim-colorizer.lua',
-    'windwp/nvim-autopairs',
-    'junegunn/vim-easy-align'
-  })
+  setup.plugin(s, {'tpope/vim-commentary', 'norcalli/nvim-colorizer.lua', 'windwp/nvim-autopairs'})
 end
 
 function L.settings(_)
   require('colorizer').setup()
   require('nvim-autopairs').setup{}
 
-end
-
-function L.keybindings(s)
-  mappings.keys(s, {{'x', 'ga', '<Plug>(EasyAlign)'}, {'n', 'ga', '<Plug>(EasyAlign)'}},
-    {silent = true, noremap = false})
 end
 
 return L
