c0b6d5a609679d57be06ea0e0b321c9b4cb4aa63
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-05-30 14:16:02 -0400

    Update comment pluggin

--- layers/dev.lua
+++ layers/dev.lua
@@ -4,10 +4,11 @@ local setup = require"LYRD.setup"
 local L = {name = 'Development'}
 
 function L.plugins(s)
-  setup.plugin(s, {'tpope/vim-commentary', 'norcalli/nvim-colorizer.lua', 'windwp/nvim-autopairs'})
+  setup.plugin(s, {'numToStr/Comment.nvim', 'norcalli/nvim-colorizer.lua', 'windwp/nvim-autopairs'})
 end
 
 function L.settings(_)
+  require('Comment').setup()
   require('colorizer').setup()
   require('nvim-autopairs').setup{}
 
