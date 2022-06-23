bb7cda64272fef3ceea5266a2b4ed8d391a165b0
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-06-23 08:56:58 -0400

    Add default formatters

--- layers/format.lua
+++ layers/format.lua
@@ -21,4 +21,17 @@ function L.add_formatters(file_type, formatters)
   custom_formatters[file_type] = formatters
 end
 
+-- Default formatters
+L.add_formatters('cs', {require('formatter.filetypes.cs').dotnetformat})
+L.add_formatters('css', {require('formatter.filetypes.css').prettier})
+L.add_formatters('go', {require('formatter.filetypes.go').gofmt})
+L.add_formatters('html', {require('formatter.filetypes.html').prettier})
+L.add_formatters('javascript', {require('formatter.filetypes.javascript').prettier})
+L.add_formatters('json', {require('formatter.filetypes.json').prettier})
+L.add_formatters('lua', {require('formatter.filetypes.lua').luaformat})
+L.add_formatters('python', {require('formatter.filetypes.python').yapf})
+L.add_formatters('toml', {require('formatter.filetypes.toml').taplo})
+L.add_formatters('typescript', {require('formatter.filetypes.typescript').prettier})
+L.add_formatters('yaml', {require('formatter.filetypes.yaml').prettier})
+
 return L
