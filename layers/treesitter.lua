e1076ebbebfd4d85efdcc7f6ed8d7d00adf9013a
Diego Ortiz <diegoortizmatajira@gmail.com>
2022-12-21 20:49:21 -0500

    Updated

--- layers/treesitter.lua
+++ layers/treesitter.lua
@@ -43,7 +43,51 @@ function L.settings(s)
 				node_decremental = "grm",
 			},
 		},
-        indent = { enable = true, disable = { 'python' } },
+		indent = { enable = true, disable = { "python" } },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>a"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<leader>A"] = "@parameter.inner",
+				},
+			},
+		},
 	})
 	require("refactoring").setup({
 		-- prompt for return type
