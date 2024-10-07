126e7e75490d3f5f332aea5448cd8f9ec6490dcd
Diego Ortiz <diegoortizmatajira@gmail.com>
2024-10-07 09:51:15 -0400

    Clipboard

--- layers/clipboard.lua
+++ layers/clipboard.lua
@@ -19,6 +19,22 @@ function L.settings(_)
 			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
 		},
 	}
+	if vim.env.TMUX ~= nil then
+		local copy = { "tmux", "load-buffer", "-w", "-" }
+		local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+		vim.g.clipboard = {
+			name = "tmux",
+			copy = {
+				["+"] = copy,
+				["*"] = copy,
+			},
+			paste = {
+				["+"] = paste,
+				["*"] = paste,
+			},
+			cache_enabled = 0,
+		}
+	end
 end
 
 return L
