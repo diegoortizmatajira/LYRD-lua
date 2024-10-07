118d1305a2fec200128326bb8cfaf7ea5d82032e
Diego Ortiz <diegoortizmatajira@gmail.com>
2024-10-07 15:52:59 -0400

    Clipboard WIP

--- layers/clipboard.lua
+++ layers/clipboard.lua
@@ -8,33 +8,35 @@ end
 
 function L.settings(_)
 	vim.opt.clipboard = "unnamed,unnamedplus"
-	vim.g.clipboard = {
-		name = "OSC 52",
-		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-		},
-		paste = {
-			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-		},
-	}
-	if vim.env.TMUX ~= nil then
-		local copy = { "tmux", "load-buffer", "-w", "-" }
-		local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
-		vim.g.clipboard = {
-			name = "tmux",
-			copy = {
-				["+"] = copy,
-				["*"] = copy,
-			},
-			paste = {
-				["+"] = paste,
-				["*"] = paste,
-			},
-			cache_enabled = 0,
-		}
-	end
+	-- vim.g.clipboard = {
+	-- 	name = "OSC 52",
+	-- 	copy = {
+	-- 		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+	-- 		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	-- 	},
+	-- 	paste = {
+	-- 		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+	-- 		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	-- 	},
+	-- }
+	-- if vim.env.TMUX ~= nil then
+	-- 	local copy = { "tmux", "load-buffer", "-w", "-" }
+	-- 	local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+	-- 	vim.g.clipboard = {
+	-- 		name = "tmux",
+	-- 		copy = {
+	-- 			["+"] = copy,
+	-- 			["*"] = copy,
+	-- 		},
+	-- 		paste = {
+	-- 			["+"] = paste,
+	-- 			["*"] = paste,
+	-- 		},
+	-- 		cache_enabled = 0,
+	-- 	}
+	-- end
+
 end
 
 return L
+
