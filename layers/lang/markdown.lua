0b86fb948ddebfc6d5baf9b63b37ab6cdeb6d266
Diego Ortiz <diegoortizmatajira@gmail.com>
2024-11-10 10:21:04 -0500

    markdown visualization

--- layers/lang/markdown.lua
+++ layers/lang/markdown.lua
@@ -4,14 +4,15 @@ local L = { name = "Markdown" }
 
 function L.plugins(s)
 	setup.plugin(s, {
-
-    })
+		{
+			"MeanderingProgrammer/markdown.nvim",
+			main = "render-markdown",
+			opts = {},
+			dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		},
+	})
 end
 
-function L.settings(s) end
-
-function L.keybindings(s) end
-
-function L.complete(s) end
+function L.settings(_) end
 
 return L
