ee53c8fb0602c4a9d517c4feaa84e0bc93623fef
Diego Ortiz <diegoortizmatajira@gmail.com>
2024-12-08 08:48:52 -0500

    Fix comments

--- layers/dev.lua
+++ layers/dev.lua
@@ -14,9 +14,12 @@ function L.plugins(s)
 		}, -- use lazy.nvim
 		{
 			"numtostr/comment.nvim",
-			opts = {
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			},
+			config = function()
+				---@diagnostic disable-next-line: missing-fields
+				require("Comment").setup({
+					pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+				})
+			end,
 			dependencies = {
 				"joosepalviste/nvim-ts-context-commentstring",
 			},
