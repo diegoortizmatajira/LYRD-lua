04371896d226597a909ce5b4cb942d6ddb191dbd
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-01-02 06:54:49 -0500

    noice buffer type as special buffer

--- layers/lyrd-ui.lua
+++ layers/lyrd-ui.lua
@@ -20,6 +20,7 @@ local L = {
 		{ filetype = "aerial", title = "Outline" },
 		{ filetype = "alpha" },
 		{ filetype = "fugitive" },
+		{ filetype = "noice" },
 		{ filetype = "qf" },
 		{ filetype = "gitcommit" },
 		{ filetype = "help" },
