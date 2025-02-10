cad6a36060c58ca828a10bed71baec766603a738
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-02-10 14:25:37 -0500

    Emmet language server

--- layers/lang/web-standard.lua
+++ layers/lang/web-standard.lua
@@ -6,70 +6,88 @@ local cmd = require("LYRD.layers.lyrd-commands").cmd
 local L = { name = "Web Standard Languages" }
 
 function L.plugins()
-    setup.plugin({
-        {
-            "pangloss/vim-javascript",
-            init = function()
-                vim.g.javascript_plugin_jsdoc = 1
-                vim.g.javascript_plugin_ngdoc = 1
-                vim.g.javascript_plugin_flow = 1
-            end,
-            ft = { "js" },
-        },
-        "b0o/schemastore.nvim",
-        {
-            "windwp/nvim-ts-autotag",
-            event = "InsertEnter",
-            dependencies = {
-                "nvim-treesitter/nvim-treesitter",
-                "windwp/nvim-autopairs",
-            },
-            opts = {},
-            ft = { "json", "yaml" },
-        },
-    })
+	setup.plugin({
+		{
+			"pangloss/vim-javascript",
+			init = function()
+				vim.g.javascript_plugin_jsdoc = 1
+				vim.g.javascript_plugin_ngdoc = 1
+				vim.g.javascript_plugin_flow = 1
+			end,
+			ft = { "js" },
+		},
+		"b0o/schemastore.nvim",
+		{
+			"windwp/nvim-ts-autotag",
+			event = "InsertEnter",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+				"windwp/nvim-autopairs",
+			},
+			opts = {},
+			ft = { "json", "yaml" },
+		},
+	})
 end
 
 function L.preparation()
-    lsp.mason_ensure({
-        "json-lsp",
-        "json-to-struct",
-        "yaml-language-server",
-        "prettier",
-    })
-    local null_ls = require("null-ls")
-    lsp.null_ls_register_sources({
-        null_ls.builtins.formatting.prettier.with({ -- For most standard file types
-            extra_filetypes = { "htmldjango" },
-        }),
-    })
+	lsp.mason_ensure({
+		"json-lsp",
+		"json-to-struct",
+		"yaml-language-server",
+		"prettier",
+		"emmet-language-server",
+	})
+	local null_ls = require("null-ls")
+	lsp.null_ls_register_sources({
+		null_ls.builtins.formatting.prettier.with({ -- For most standard file types
+			extra_filetypes = { "htmldjango" },
+		}),
+	})
 end
 
 function L.complete()
-    lsp.enable("jsonls", {
-        settings = {
-            json = {
-                schemas = require("schemastore").json.schemas(),
-                validate = {
-                    enabled = true,
-                },
-            },
-        },
-    })
-    lsp.enable("yamlls", {
-        settings = {
-            yaml = {
-                hover = true,
-                completion = true,
-                validate = true,
-                schemaStore = {
-                    enable = true,
-                    url = "https://www.schemastore.org/api/json/catalog.json",
-                },
-                schemas = require("schemastore").yaml.schemas(),
-            },
-        },
-    })
+	lsp.enable("jsonls", {
+		settings = {
+			json = {
+				schemas = require("schemastore").json.schemas(),
+				validate = {
+					enabled = true,
+				},
+			},
+		},
+	})
+	lsp.enable("yamlls", {
+		settings = {
+			yaml = {
+				hover = true,
+				completion = true,
+				validate = true,
+				schemaStore = {
+					enable = true,
+					url = "https://www.schemastore.org/api/json/catalog.json",
+				},
+				schemas = require("schemastore").yaml.schemas(),
+			},
+		},
+	})
+	lsp.enable("emmet_language_server", {
+		settings = {
+			filetypes = {
+				"css",
+				"eruby",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"less",
+				"sass",
+				"scss",
+				"pug",
+				"typescriptreact",
+				"vue",
+			},
+		},
+	})
 end
 
 return L
