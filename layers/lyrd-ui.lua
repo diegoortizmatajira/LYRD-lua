1209bfe32aa1e1a6fdb68cf89996c77ef230694c
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-02-17 12:43:56 -0500

    Fix plugin identifier in lowercase

--- layers/lyrd-ui.lua
+++ layers/lyrd-ui.lua
@@ -102,7 +102,7 @@ function L.plugins()
 			end,
 			dependencies = {
 				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-				"MunifTanjim/nui.nvim",
+				"muniftanjim/nui.nvim",
 				-- OPTIONAL:
 				--   `nvim-notify` is only needed, if you want to use the notification view.
 				--   If not available, we use `mini` as the fallback
