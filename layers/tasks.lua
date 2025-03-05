4bfc3cb25a1601eff5471d4dd3ca433c783c55f5
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-03-04 22:33:40 -0500

    Remove projector

--- layers/tasks.lua
+++ layers/tasks.lua
@@ -17,58 +17,12 @@ function L.plugins()
 					default_detail = 1,
 				},
 			},
-			lazy = true,
-		},
-		{
-			"kndndrj/nvim-projector",
-			dependencies = {
-				-- required:
-				"muniftanjim/nui.nvim",
-				"kndndrj/projector-neotest",
-				"kndndrj/projector-vscode",
-			},
-			config = function()
-				require("projector").setup({
-					loaders = {
-						require("projector_vscode").LaunchJsonLoader:new(),
-						require("projector_vscode").TasksJsonLoader:new(),
-						require("projector.loaders").BuiltinLoader:new(),
-						require("projector.loaders").DapLoader:new(),
-					},
-					outputs = {
-						require("projector.outputs").TaskOutputBuilder:new(),
-						require("projector.outputs").DadbodOutputBuilder:new(),
-						require("projector.outputs").DapOutputBuilder:new(),
-						require("projector_neotest").OutputBuilder:new(),
-					},
-				})
-			end,
-			lazy = true,
-		},
-		{
-			"kndndrj/projector-neotest",
-			dependencies = {
-				"nvim-neotest/neotest",
-			},
-			lazy = true,
-		},
-		{
-			"kndndrj/projector-vscode",
-			lazy = true,
 		},
 	})
 end
 
 function L.settings()
-	local debug = require("LYRD.layers.debug")
-	commands.implement("*", {
-		{
-			cmd.LYRDDebugStart,
-			debug.start_handler(function()
-				require("projector").continue()
-			end),
-		},
-	})
+	commands.implement("*", {})
 end
 
 return L
