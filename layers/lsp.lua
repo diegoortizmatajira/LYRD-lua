5576487f400dc5ac213f21e12772bb74c5d2803c
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-06-12 20:35:08 -0400

    Dotnet formatter

--- layers/lsp.lua
+++ layers/lsp.lua
@@ -297,6 +297,26 @@ function L.settings()
 
 	require("conform").setup({
 		formatters_by_ft = L.conform_formatters,
+		formatters = {
+			--- TODO: Remove when conform supports this
+			csharpier = function()
+				local useDotnet = not vim.fn.executable("csharpier")
+				local command = useDotnet and "dotnet csharpier" or "csharpier"
+				local version_out = vim.fn.system(command .. " --version")
+				--NOTE: system command returns the command as the first line of the result, need to get the version number on the final line
+				local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
+				local is_new = major_version >= 1
+
+				local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
+
+				return {
+					command = command,
+					args = args,
+					stdin = not is_new,
+					require_cwd = false,
+				}
+			end,
+		},
 		-- Set default options
 		default_format_opts = {
 			lsp_format = "fallback",
