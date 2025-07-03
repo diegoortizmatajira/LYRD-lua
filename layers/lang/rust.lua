c30c3afaebac10c90f08928e0e72eafd724bcdce
Diego Ortiz <diego.ortiz@base1.com>
2025-07-03 17:03:34 -0400

    Rust LSP fixed

--- layers/lang/rust.lua
+++ layers/lang/rust.lua
@@ -4,7 +4,13 @@ local lsp = require("LYRD.layers.lsp")
 local L = { name = "Rust Language" }
 
 function L.plugins()
-	setup.plugin({})
+	setup.plugin({
+		{
+			"mrcjkb/rustaceanvim",
+			version = "^6", -- Recommended
+			lazy = false, -- This plugin is already lazy
+		},
+	})
 end
 
 function L.preparation()
@@ -15,12 +21,6 @@ end
 
 function L.settings() end
 
-function L.complete()
-	lsp.enable("rust_analyzer", {
-		-- standalone file support
-		-- setting it to false may improve startup time
-		standalone = true,
-	})
-end
+function L.complete() end
 
 return L
