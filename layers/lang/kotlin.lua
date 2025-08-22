4b7899ec226e772141bb187b991d194f06416d51
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-08-22 17:03:30 -0400

    Improvements

--- layers/lang/kotlin.lua
+++ layers/lang/kotlin.lua
@@ -10,6 +10,11 @@ end
 function L.preparation()
 	lsp.mason_ensure({
 		"kotlin-language-server",
+		"ktlint",
+	})
+	lsp.format_with_conform("kotlin", { "ktlint" })
+	lsp.null_ls_register_sources({
+		require("null-ls.builtins.diagnostics.ktlint"),
 	})
 	local ts = require("LYRD.layers.treesitter")
 	ts.ensureParser({
@@ -17,7 +22,51 @@ function L.preparation()
 	})
 end
 
-function L.settings() end
+function L.settings()
+	local dap = require("dap")
+
+	if not dap.adapters.kotlin then
+		dap.adapters.kotlin = {
+			type = "executable",
+			command = "kotlin-debug-adapter",
+			options = { auto_continue_if_many_stopped = false },
+		}
+	end
+
+	dap.configurations.kotlin = {
+		{
+			type = "kotlin",
+			request = "launch",
+			name = "This file",
+			-- may differ, when in doubt, whatever your project structure may be,
+			-- it has to correspond to the class file located at `build/classes/`
+			-- and of course you have to build before you debug
+			mainClass = function()
+				local root = vim.fs.find("src", { path = vim.uv.cwd(), upward = true, stop = vim.env.HOME })[1] or ""
+				local fname = vim.api.nvim_buf_get_name(0)
+				-- src/main/kotlin/websearch/Main.kt -> websearch.MainKt
+				return fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1)
+			end,
+			projectRoot = "${workspaceFolder}",
+			jsonLogFile = "",
+			enableJsonLogging = false,
+		},
+		{
+			-- Use this for unit tests
+			-- First, run
+			-- ./gradlew --info cleanTest test --debug-jvm
+			-- then attach the debugger to it
+			type = "kotlin",
+			request = "attach",
+			name = "Attach to debugging session",
+			port = 5005,
+			args = {},
+			projectRoot = vim.fn.getcwd,
+			hostName = "localhost",
+			timeout = 2000,
+		},
+	}
+end
 
 function L.keybindings() end
 
--- runtime/ftdetect/docker-compose.vim
+++ runtime/ftdetect/docker-compose.vim
@@ -1,4 +1,4 @@
 " Detect docker-compose.yaml as docker-compose filetype
-au BufRead,BufNewFile docker-compose*.yaml set filetype=docker-compose
-au BufRead,BufNewFile docker-compose*.yml set filetype=docker-compose
+au BufRead,BufNewFile docker-compose*.yaml set filetype=yaml.docker-compose
+au BufRead,BufNewFile docker-compose*.yml set filetype=yaml.docker-compose
 
