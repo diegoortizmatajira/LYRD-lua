017562bffd974ba82c7b56312f294e60a8cc35aa
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-08-27 12:47:02 -0400

    Hop for f,F,t and T movements

--- layers/motions.lua
+++ layers/motions.lua
@@ -49,6 +49,8 @@ function L.plugins()
 end
 
 function L.keybindings()
+	local hop = require("hop")
+	local directions = require("hop.hint").HintDirection
 	mappings.keys({
 		{ { "n", "v" }, "<Leader>w", c("HopWord"), { desc = "Go to word" } },
 		{ { "n", "v" }, "gl", c("HopLine"), { desc = "Go to line" } },
@@ -56,6 +58,18 @@ function L.keybindings()
 		{ { "n", "v" }, "s", "<cmd>HopChar1<CR>" },
 		{ { "n", "v" }, "S", "<cmd>HopChar2<CR>" },
 	})
+	vim.keymap.set("", "f", function()
+		hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+	end, { remap = true })
+	vim.keymap.set("", "F", function()
+		hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+	end, { remap = true })
+	vim.keymap.set("", "t", function()
+		hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+	end, { remap = true })
+	vim.keymap.set("", "T", function()
+		hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+	end, { remap = true })
 end
 
 return L
