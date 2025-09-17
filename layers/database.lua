39c4c3f4dcb9adbe21cf59cfd97e2ae012db92bf
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-09-17 14:58:48 -0400

    Enable CsvView on DB Results

--- layers/database.lua
+++ layers/database.lua
@@ -8,8 +8,42 @@ function L.plugins()
 	setup.plugin({
 		{
 			"diegoortizmatajira/db-cli-adapter.nvim",
-			-- dir = "/home/diegoortizmatajira/Development/contrib/db-cli-adapter.nvim",
-			opts = {},
+			dir = "/home/diegoortizmatajira/Development/contrib/db-cli-adapter.nvim",
+			--- @class DbCliAdapter.Config
+			opts = {
+				output = {
+					csv = {
+						after_query_callback = function(bufnr)
+							--- Enable CsvView for the output buffer if the output is in CSV format'
+							vim.api.nvim_buf_call(bufnr, function()
+								vim.cmd("CsvViewEnable")
+							end)
+						end,
+					},
+				},
+			},
+		},
+		{
+			"hat0uma/csvview.nvim",
+			---@module "csvview"
+			---@type CsvView.Options
+			opts = {
+				parser = { comments = { "#", "//" } },
+				keymaps = {
+					-- Text objects for selecting fields
+					textobject_field_inner = { "if", mode = { "o", "x" } },
+					textobject_field_outer = { "af", mode = { "o", "x" } },
+					-- Excel-like navigation:
+					-- Use <Tab> and <S-Tab> to move horizontally between fields.
+					-- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+					-- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+					jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+					jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+					jump_next_row = { "<Enter>", mode = { "n", "v" } },
+					jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+				},
+			},
+			cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
 		},
 	})
 end
