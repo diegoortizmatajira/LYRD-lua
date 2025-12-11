93cdc56fb52674b149fa71f350e36ec34b0b7179
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-12-11 16:30:21 -0500

    Python basedpyright or pyright

--- layers/lang/python.lua
+++ layers/lang/python.lua
@@ -5,7 +5,10 @@ local cmd = require("LYRD.layers.lyrd-commands").cmd
 local utils = require("LYRD.utils")
 
 ---@class LYRD.layer.lang.Python: LYRD.setup.Module
-local L = { name = "Python language" }
+local L = {
+	name = "Python language",
+	use_basedpyright = true,
+}
 
 -- Opens the .env file in the current directory
 local function open_dotenv()
@@ -18,7 +21,7 @@ function L.plugins()
 			"mfussenegger/nvim-dap-python",
 			dependencies = { "mfussenegger/nvim-dap" },
 			config = function()
-			    -- WARN: Using 'uv' for uv projects support, otherwise skip.
+				-- WARN: Using 'uv' for uv projects support, otherwise skip.
 				require("dap-python").setup("uv")
 				table.insert(require("dap").configurations.python, {
 					type = "python",
@@ -61,10 +64,14 @@ function L.plugins()
 end
 
 function L.preparation()
+	local python_lsp = "pyright"
+	if L.use_basedpyright then
+		python_lsp = "basedpyright"
+	end
 	lsp.mason_ensure({
+		python_lsp,
 		"debugpy",
 		"pylint",
-		"pyright",
 		"pydocstyle",
 		"ruff",
 		"yapf",
@@ -81,19 +88,6 @@ function L.preparation()
 	lsp.null_ls_register_sources({
 		-- Custom pylint to use the module in the environment (instead of the Mason one). Requires to install pylint manually.
 		null_ls.builtins.diagnostics.pylint,
-		-- null_ls.builtins.diagnostics.pylint.with({
-		-- 	command = "python",
-		-- 	args = {
-		-- 		"-m",
-		-- 		"pylint",
-		-- 		"--rcfile",
-		-- 		utils.join_paths(setup.configs_path, "pylintrc"),
-		-- 		"--from-stdin",
-		-- 		"$FILENAME",
-		-- 		"-f",
-		-- 		"json",
-		-- 	},
-		-- }),
 	})
 	local test = require("LYRD.layers.test")
 	test.configure_adapter(require("neotest-python"))
@@ -118,7 +112,11 @@ function L.settings()
 end
 
 function L.complete()
-	vim.lsp.enable({ "pyright", "ruff" })
+	local python_lsp = "pyright"
+	if L.use_basedpyright then
+		python_lsp = "basedpyright"
+	end
+	vim.lsp.enable({ python_lsp, "ruff" })
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
 		callback = function(args)
