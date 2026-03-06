ea2145c3b3c1d31bf983efce9e647a03d2a6724b
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-03-05 23:11:46 -0500

    Refactor Markdown layer

--- layers/lang/markdown.lua
+++ layers/lang/markdown.lua
@@ -1,12 +1,46 @@
 local setup = require("LYRD.setup")
 local icons = require("LYRD.layers.icons")
-local commands = require("LYRD.layers.commands")
-local cmd = require("LYRD.layers.lyrd-commands").cmd
 
 ---@class LYRD.layer.lang.Markdown: LYRD.setup.Module
 local L = {
 	name = "Markdown",
-	max_line_length = 80,
+	--- Defines custom formatters, order matters as they are applied in the order they are defined here.
+	custom_formatters = {
+		--- Configures a custom formatter for Markdown files that uses prettier
+		--- with specific arguments to ensure that prose is wrapped and the print
+		--- width is set to a specified value, enhancing readability and formatting
+		--- consistency for Markdown content.
+		["prettier_markdown"] = {
+			inherit = "prettier",
+			prepend_args = { "--prose-wrap", "always", "--print-width", tostring(80) },
+		},
+
+		--- Configures the markdown-toc formatter to only run when the buffer contains
+		--- the string "<!-- toc -->", and if it does, it will generate a table of
+		--- contents with bullet points using the "-" character.
+		["markdown-toc"] = {
+			condition = function(_, ctx)
+				for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+					if line:find("<!%-%- toc %-%->") then
+						return true
+					end
+				end
+			end,
+			args = { "--bullets", "-", "-i", "$FILENAME" },
+		},
+
+		--- Configures the markdownlint-cli2 formatter to only run when there are
+		--- diagnostics in the buffer that have "markdownlint-cli2" as their source, ensuring
+		--- that the formatter only runs when there are relevant issues to address.
+		["markdownlint-cli2"] = {
+			condition = function(_, ctx)
+				local diag = vim.tbl_filter(function(d)
+					return d.source == "markdownlint-cli2"
+				end, vim.diagnostic.get(ctx.buf))
+				return #diag > 0
+			end,
+		},
+	},
 }
 
 function L.plugins()
@@ -44,7 +78,7 @@ function L.plugins()
 			dependencies = {
 				"nvim-treesitter/nvim-treesitter",
 				"nvim-tree/nvim-web-devicons",
-			}, -- if you prefer nvim-web-devicons
+			},
 		},
 		{
 			"Nedra1998/nvim-mdlink",
@@ -77,47 +111,19 @@ function L.preparation()
 		"html",
 		"yaml",
 	})
-	--- Configures the markdown-toc formatter to only run when the buffer contains
-	--- the string "<!-- toc -->", and if it does, it will generate a table of
-	--- contents with bullet points using the "-" character.
-	lsp.customize_formatter("markdown-toc", {
-		condition = function(_, ctx)
-			for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-				if line:find("<!%-%- toc %-%->") then
-					return true
-				end
-			end
-		end,
-		args = { "--bullets", "-", "-i", "$FILENAME" },
-	})
-	--- Configures a custom formatter for Markdown files that uses prettier
-	--- with specific arguments to ensure that prose is wrapped and the print
-	--- width is set to the value of L.max_line_length.
-	local PRETTIER_MARKDOWN = "prettier_markdown"
-	lsp.customize_formatter(PRETTIER_MARKDOWN, {
-		inherit = "prettier",
-		prepend_args = { "--prose-wrap", "always", "--print-width", tostring(L.max_line_length) },
-	})
-	--- Configures the markdownlint-cli2 formatter to only run when there are
-	--- diagnostics in the buffer that have "markdownlint-cli2" as their source, ensuring
-	--- that the formatter only runs when there are relevant issues to address.
-	lsp.customize_formatter("markdownlint-cli2", {
-		condition = function(_, ctx)
-			local diag = vim.tbl_filter(function(d)
-				return d.source == "markdownlint-cli2"
-			end, vim.diagnostic.get(ctx.buf))
-			return #diag > 0
-		end,
-	})
-	--- Configures multiple formatters for markdown files.
-	lsp.format_with_conform({
-		"markdown",
-		"markdown.mdx",
-	}, {
-		PRETTIER_MARKDOWN,
-		"markdownlint-cli2",
-		"markdown-toc",
-	}, nil)
+	--- Iterates over the custom formatters defined in the `custom_formatters` table and
+	--- registers each formatter with the LSP layer, allowing them to be used for formatting
+	--- Markdown files according to the specified conditions and arguments defined for each formatter.
+	for key, value in pairs(L.custom_formatters) do
+		lsp.customize_formatter(key, value)
+	end
+	--- Configures the list of formatters to be applied to Markdown files,
+	--- ensuring that the custom formatters defined in the `custom_formatters`
+	--- table are used when formatting Markdown content.
+	lsp.format_with_conform({ "markdown", "markdown.mdx" }, vim.tbl_keys(L.custom_formatters))
+	--- Registers the markdownlint_cli2 diagnostic source with null-ls, enabling
+	--- linting for Markdown files using the markdownlint-cli2 tool, which helps
+	--- identify and fix issues in Markdown content according to the rules defined by markdownlint.
 	lsp.null_ls_register_sources({
 		require("null-ls.builtins.diagnostics.markdownlint_cli2"),
 	})
