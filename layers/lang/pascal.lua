02bad7a401791c9ce0f517306f5dfc76f1fe2c41
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-03-11 06:16:57 -0400

    Cleaning pascal layer

--- layers/lang/pascal.lua
+++ layers/lang/pascal.lua
@@ -1,4 +1,3 @@
-local icons = require("LYRD.layers.icons")
 local declarative_layer = require("LYRD.shared.declarative_layer")
 
 --- @type table|LYRD.setup.DeclarativeLayer
@@ -21,24 +20,5 @@ local L = {
 	required_formatter_per_filetype = {},
 	required_test_adapters = {},
 }
-
-function L.plugins()
-	local setup = require("LYRD.setup")
-	setup.plugin({})
-end
-
-function L.preparation() end
-
-function L.settings()
-	local commands = require("LYRD.layers.commands")
-	local cmd = require("LYRD.layers.lyrd-commands").cmd
-	commands.implement("*", {
-		-- { cmd.LYRDXXXX, ":XXXXX" },
-	})
-end
-
-function L.keybindings() end
-
-function L.complete() end
 
 return declarative_layer.apply(L)
