d7aa2b39cf2324369b01b928ba37fd88ea36f861
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-03-26 18:32:02 -0400

    (feature) dotenv-lint

--- layers/lang/python.lua
+++ layers/lang/python.lua
@@ -60,6 +60,7 @@ local L = {
 		"pylint",
 		"ruff",
 		"yapf",
+		"dotenv-linter",
 	},
 	required_treesitter_parsers = {
 		"python",
@@ -76,6 +77,9 @@ local L = {
 		declarative_layer.source_with_opts("null-ls.builtins.diagnostics.pylint", {
 			command = "python",
 			args = { "-m", "pylint", "--from-stdin", "$FILENAME", "-f", "json" },
+		}),
+		declarative_layer.source_with_opts("null-ls.builtins.diagnostics.dotenv_linter", {
+			args = { "check", "$FILENAME" },
 		}),
 	},
 }
