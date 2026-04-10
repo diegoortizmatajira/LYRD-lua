91a0755a08c8e9bfb7527a68a73830069d6b046f
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-04-10 14:45:44 -0400

    refactor: Move and integrate live-server task setup for dev server
    - Remove redundant `run_local_server` and `StartDevServer` from `web-standard.lua` to streamline functionality.
    - Add `run_local_server` and `StartDevServer` to `dev.lua` for centralized server task setup.

--- layers/dev.lua
+++ layers/dev.lua
@@ -1,6 +1,4 @@
 local setup = require("LYRD.setup")
-local commands = require("LYRD.layers.commands")
-local cmd = require("LYRD.layers.lyrd-commands").cmd
 
 ---@class LYRD.layer.Development: LYRD.setup.Module
 local L = { name = "Development Tools" }
@@ -41,8 +39,52 @@ function L.plugins()
 	})
 end
 
+local function run_local_server(port, folder)
+	local command = "live-server"
+	if vim.fn.executable(command) == 0 then
+		vim.notify(
+			"live-server is not installed. Please install it globally using 'npm i -g live-server'",
+			vim.log.levels.ERROR
+		)
+		return
+	end
+	local tasks = require("LYRD.layers.tasks")
+	tasks.run_task({
+		name = string.format("Developer Local Server (port: %s)", port),
+		cmd = command,
+		args = {
+			string.format("--port=%s", port),
+			folder,
+		},
+		open_in_split = true,
+		focus = true,
+	})
+end
+
+function L.StartDevServer()
+	vim.ui.input({ prompt = "Enter port number:", default = "3000" }, function(port)
+		local current_dir = vim.fn.getcwd()
+		vim.ui.input(
+			{ prompt = "Folder to serve (leave blank for current directory):", default = current_dir },
+			function(folder)
+				-- Check if the provided folder is valid
+				if folder == "" then
+					folder = current_dir
+				elseif not vim.fn.isdirectory(folder) then
+					vim.notify("Invalid folder path: " .. folder, vim.log.levels.ERROR)
+					return
+				end
+				run_local_server(port, folder)
+			end
+		)
+	end)
+end
+
 function L.settings()
+	local commands = require("LYRD.layers.commands")
+	local cmd = require("LYRD.layers.lyrd-commands").cmd
 	commands.implement("*", {
+		{ cmd.LYRDDevServerStart, L.StartDevServer },
 		{
 			cmd.LYRDSearchMacros,
 			function()
