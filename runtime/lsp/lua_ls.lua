981c18c33f01d8d0ef94e03980ca7b6483e32574
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-06-03 21:33:57 -0400

    feat: Update Lua LSP configuration for improved workspace support
    - Add `path` settings in `runtime` to include specific Lua file patterns.
    - Replace `diagnostics.globals` with `library_files = false` for diagnostics.
    - Add `workspace.library` paths to enhance external library support.

--- runtime/lsp/lua_ls.lua
+++ runtime/lsp/lua_ls.lua
@@ -3,18 +3,26 @@ return {
 		Lua = {
 			runtime = {
 				version = "LuaJIT",
+				path = {
+					"lua/?.lua",
+					"lua/?/init.lua",
+				},
 			},
 			diagnostics = {
-				globals = {
-					"vim",
-					"awesome",
-					"client",
-					"mouse",
-					"require",
-				},
+				library_files = false,
 			},
 			telemetry = {
 				enable = false,
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					["/github/workspace/deps/neodev.nvim/types/stable"] = true,
+					["${3rd}/busted/library"] = true,
+					["${3rd}/luassert/library"] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
 			},
 		},
 	},
